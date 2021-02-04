65c1732b7812ab6e6f0af0b1aa13b1656b12f407
Jack Miras <jackmiras@gmail.com>
2021-02-03 23:05:53 -0300

    Fixing name of the rule files for phpcs and phpmd

--- .gitlab-ci/scripts/linters.sh
+++ .gitlab-ci/scripts/linters.sh
@@ -7,18 +7,18 @@ echo "# PHP_CodeSniffer                                                         
 echo "################################################################################"
 
 echo "" && ./vendor/bin/phpcs \
-	--cache \
-	--colors \
-	--extensions=php \
-	--warning-severity=0 \
-	--runtime-set ignore_warnings_on_exit true \
-	--standard=phpcs.xml app/ routes/ config/ database/
+  --cache \
+  --colors \
+  --extensions=php \
+  --warning-severity=0 \
+  --runtime-set ignore_warnings_on_exit true \
+  --standard=phpcs-rules.xml app/ routes/ config/ database/
 
 echo $'\n################################################################################'
 echo "# PHP Mess Detector                                                            #"
 echo "################################################################################"
 
-./vendor/bin/phpmd app/ ansi phpmd.xml
-./vendor/bin/phpmd config/ ansi phpmd.xml
-./vendor/bin/phpmd routes/ ansi phpmd.xml
-./vendor/bin/phpmd database/ ansi phpmd.xml && echo ""
+./vendor/bin/phpmd app/ ansi phpmd-rules.xml
+./vendor/bin/phpmd config/ ansi phpmd-rules.xml
+./vendor/bin/phpmd routes/ ansi phpmd-rules.xml
+./vendor/bin/phpmd database/ ansi phpmd-rules.xml && echo ""

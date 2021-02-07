138f37cbb4bb3e2ef6cf5e5547b3fe0b22433ddd
Jack Miras <jackmiras@gmail.com>
2021-02-07 12:29:20 -0300

    Fixing translation and test for ModelUpdatingException

--- tests/Unit/Exceptions/ModelUpdatingExceptionTest.php
+++ tests/Unit/Exceptions/ModelUpdatingExceptionTest.php
@@ -9,30 +9,30 @@ use App\Exceptions\ModelUpdatingException;
 beforeEach(function () {
     $this->id = 1;
     $this->model = 'App\Models\User';
-    $this->modelUpdatingException = new ModelUpdatingException($this->id, $this->model);
+    $this->exception = new ModelUpdatingException($this->id, $this->model);
 });
 
 it('expects render to return a response instance when invoked', function () {
-    $response = $this->modelUpdatingException->render(request());
+    $response = $this->exception->render(request());
 
     expect($response)->toBeInstanceOf(Response::class);
 });
 
 it('expects status code 400 when expection is thrown', function () {
-    $status = $this->modelUpdatingException->status();
+    $status = $this->exception->status();
 
     expect($status)->toBe(Response::HTTP_BAD_REQUEST);
 });
 
 it('expects help to be anonymous help when expection is thrown', function () {
-    $help = $this->modelUpdatingException->help();
+    $help = $this->exception->help();
 
     expect($help)->toBe(trans('exception.model_not_updated.help'));
 });
 
 it('expects error to be anonymous error when expection is thrown', function () {
-    $error = $this->modelUpdatingException->error();
+    $error = $this->exception->error();
     $replace = ['id' => $this->id, 'model' => Str::afterLast($this->model, '\\')];
 
-    expect($error)->toBe(trans('exception.model_not_updated.message', $replace));
+    expect($error)->toBe(trans('exception.model_not_updated.error', $replace));
 });
